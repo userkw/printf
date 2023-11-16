9979e27d9c03055bb2f8aaa8cd10e208bc9c4d64
StarpowerNaledi <n.mokua972@gmail.com>
2023-11-16 14:34:03 -0800

    Add extensive printf funtion

---
+++ 0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+int main (void)
+{
+	char c;
+	char* s;
+	int d;
+	int c;
+
+	_printf("%c\n", Naledi);
+	_printf("%s\n",'The baby can code');
+	_printf("%d\n", 26);
+	_printf("%i", 30);
+
+return (0);
+}
